4f3ac25d12efc9097b034253190ac43c6d1cc37d
xiaoyu0226 <emilyyu0226@gmail.com>
2025-01-18 22:42:31 -0800

    add request sender
    1. destroy cam video cap videos
    2. made requestSender

---
+++ requestSender.py
@@ -0,0 +1,44 @@
+import requests
+from requests.exceptions import HTTPError, ConnectionError, Timeout, RequestException
+
+class RequestSender:
+    def __init__(self, server_url):
+        self.server_url = server_url
+
+    def _make_request(self, endpoint, data, method='POST'):    # make this private
+        try:
+            if method == 'POST':
+                response = requests.post(f'{self.server_url}/{endpoint}', data=data)
+            elif method == 'GET':
+                response = requests.get(f'{self.server_url}/{endpoint}', params=data)
+            else:
+                raise ValueError("Unsupported HTTP method: Use 'POST' or 'GET'.")
+            
+            # Raise exception for bad status codes (4xx, 5xx)
+            response.raise_for_status()
+            return response.json()
+        
+        except HTTPError as http_err:
+            return {"error": f"HTTP error occurred: {http_err}"}
+        except ConnectionError as conn_err:
+            return {"error": f"Connection error occurred: {conn_err}"}
+        except Timeout as timeout_err:
+            return {"error": f"Timeout error occurred: {timeout_err}"}
+        except RequestException as req_err:
+            return {"error": f"An error occurred: {req_err}"}
+        except Exception as err:
+            return {"error": f"Unexpected error: {err}"}
+
+    def check_user(self, email):
+        email_data = {'email': email}
+        return self._make_request('check-user', email_data)
+
+    def registration(self, email, password):
+        register_data = {'email': email, 'password': password}
+        return self._make_request('register', register_data)
+
+    def login(self, email, password):
+        login_data = {'email': email, 'password': password}
+        return self._make_request('login', login_data)
+
+
