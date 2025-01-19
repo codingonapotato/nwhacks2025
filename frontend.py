07500c48ef32a810edecc23b7f1fd26e147c4ef7
ethanngu <ethanngu19@gmail.com>
2025-01-18 19:05:17 -0800

    Updated frontend to use QStackedWidget
    [1] Changed frontend to use QStacked widget
    [2] built second page for username checking

--- frontend.py
+++ frontend.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget, QStackedWidget
 import sys
 
 class MainWindow(QWidget):
@@ -8,7 +8,15 @@ class MainWindow(QWidget):
         
         self.resize(1000,600)
         
+        # init stacked widget
+        self.stackedWidget = QStackedWidget()
+        
         self.setMainMenu()
+        
+        # Set up the stacked widget as the first one 
+        init_Layout = QVBoxLayout()
+        init_Layout.addWidget(self.stackedWidget)
+        self.setLayout(init_Layout)
         
         
 
@@ -18,40 +26,48 @@ class MainWindow(QWidget):
     
     # Function to set the Main Menu (initial screen)    
     def setMainMenu(self):
-        self.mainLayout = QVBoxLayout() # Create vertical box layout
+        mainMenuWidget = QWidget()
+        mainLayout = QVBoxLayout()
         # Log In Flow 
-        self.loginLabel = QLabel("Returning users can log in below:")
-        self.mainLayout.addWidget(self.loginLabel)
-        self.loginFlowButton = QPushButton("Log In")
-        # self.submit_button.clicked.connect(self.submit)
-        self.mainLayout.addWidget(self.loginFlowButton)
+        loginLabel = QLabel("Returning users can log in below:")
+        mainLayout.addWidget(loginLabel)
+        loginFlowButton = QPushButton("Log In")
+        loginFlowButton.clicked.connect(self.setLoginPageUsername)
+        mainLayout.addWidget(loginFlowButton)
         
         # Register Flow Label
-        self.registerLabel = QLabel("New User? Create an account:")
-        self.mainLayout.addWidget(self.registerLabel)
-        self.registerFlowButton = QPushButton("Register")
-        self.mainLayout.addWidget(self.registerFlowButton)
+        registerLabel = QLabel("New User? Create an account:")
+        mainLayout.addWidget(registerLabel)
+        registerFlowButton = QPushButton("Register")
+        mainLayout.addWidget(registerFlowButton)
+        
+        mainMenuWidget.setLayout(mainLayout)
+        
+        self.stackedWidget.addWidget(mainMenuWidget)
 
-        self.setLayout(self.mainLayout) # important 
+        # self.setLayout(self.mainLayout) # important 
     
     
     # Function to set the Login page to enter username     
     def setLoginPageUsername(self):
-        self.mainLayout = QVBoxLayout() # Create vertical box layout
-        # Log In Flow 
-        self.loginLabel = QLabel("Returning users can log in below:")
-        self.mainLayout.addWidget(self.loginLabel)
-        self.loginFlowButton = QPushButton("Log In")
-        # self.submit_button.clicked.connect(self.submit)
-        self.mainLayout.addWidget(self.loginFlowButton)
+        loginWidget_1 = QWidget()
+        login1 = QVBoxLayout() # Create vertical box layout
         
-        # Register Flow Label
-        self.registerLabel = QLabel("New User? Create an account:")
-        self.mainLayout.addWidget(self.registerLabel)
-        self.registerFlowButton = QPushButton("Register")
-        self.mainLayout.addWidget(self.registerFlowButton)
+        userLabel = QLabel("Enter Email below")
+        login1.addWidget(userLabel)
+        usernameField = QLineEdit()
+        login1.addWidget(usernameField)
+        loginFlowButton = QPushButton("Next")
+        login1.addWidget(loginFlowButton)
+        # self.submit_button.clicked.connect(self.submit)
 
-        self.setLayout(self.mainLayout) # important 
+        loginWidget_1.setLayout(login1) 
+        
+        self.stackedWidget.addWidget(loginWidget_1)
+        
+        self.stackedWidget.setCurrentWidget(loginWidget_1)
+        
+    def 
 
 
 
