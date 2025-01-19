f7ae24c5dbba29f4d97eef98181602c8ad42124e
codingonapotato <hc2020@students.ubc.ca>
2025-01-18 23:03:21 -0800

    fixed bug with register endpoint

--- server/flaskr/__init__.py
+++ server/flaskr/__init__.py
@@ -24,7 +24,7 @@ def create_app(test_config=None):
     @app.route('/register', methods=['POST'])
     def registerUser():
         data = request.get_json()
-        email, password = data # TODO: Decrypt with private key
+        email, password = data["email"], data["password"] # TODO: Decrypt with private key
         response = {
             "message" : {"email" : email, "password" : password}, 
             "status" : 404
@@ -49,7 +49,7 @@ def create_app(test_config=None):
     @app.route('/login', methods=['POST'])
     def login():
         data = request.get_json()
-        email, password = data # TODO: Decrypt with private key
+        email, password = data["email"], data["password"] # TODO: Decrypt with private key
         response = {
             "message" : {"email" : email, "password" : password}, 
             "status" : 404
